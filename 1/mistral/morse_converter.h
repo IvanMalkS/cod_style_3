d5afbfa4616bc7311119fa1b37e4125dc3cfed95
Van1s1mys <77716298+IvanMalkS@users.noreply.github.com>
2025-04-24 20:49:08 +0500

    Create morse_converter.h

---
+++ 1/mistral/morse_converter.h
@@ -0,0 +1,11 @@
+/* Продолжение задания №Программа-телеграф */
+
+#ifndef MORSE_CONVERTER_H
+#define MORSE_CONVERTER_H
+
+#include <string>
+
+// Функция для перевода текста в азбуку Морзе
+std::string textToMorse(const std::string& text);
+
+#endif // MORSE_CONVERTER_H
