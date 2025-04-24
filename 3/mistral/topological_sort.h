ea69a577793bcecb3e1cf7700c021fde403df35c
Van1s1mys <77716298+IvanMalkS@users.noreply.github.com>
2025-04-24 22:17:53 +0500

    Create topological_sort.h

---
+++ 3/mistral/topological_sort.h
@@ -0,0 +1,11 @@
+/* Продолжение задания №Топологическая сортировка графа */
+
+#ifndef TOPOLOGICAL_SORT_H
+#define TOPOLOGICAL_SORT_H
+
+#include <vector>
+
+// Функция для выполнения топологической сортировки
+std::vector<int> topologicalSort(const std::vector<std::vector<int>>& adjMatrix);
+
+#endif // TOPOLOGICAL_SORT_H
